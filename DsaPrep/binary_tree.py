e0150044a0ef1d0b5d6d2f89d9655487836d46af
Nandini Gupta <nsg2001nsg@gmail.com>
2025-07-28 21:32:26 +0530

    implemented inorder, preorder and postorder traversal for simple binary tree

--- DsaPrep/binary_tree.py
+++ DsaPrep/binary_tree.py
@@ -71,14 +71,106 @@ class BinaryTree:
                 node = q.popleft()
         print("Tree is Empty!")
 
+    def inorder(self):  # Left Root Right
+        if self.root:
+            node = self.root
+            q = deque()
+            flag = 1
+            while node:
+                if node.left:
+                    if flag:
+                        q.appendleft(node)
+                        node = node.left
+                        flag = 1
+                        continue
+                print(node.data, end=" ")
+                if node.right:
+                    node = node.right
+                    flag = 1
+                else:
+                    try:
+                        node = q.popleft()
+                    except Exception:
+                        return
+                    flag = 0
+        else:
+            print("Tree is Empty!")
 
-b = BinaryTree()
-b.display()
-b.insert(1)
-b.insert(2)
-b.insert(3)
-b.insert(4)
-b.insert(5)
-b.insert(6)
-b.insert(7)
-b.display()
+    def preorder(self):  # Root Left Right
+        if self.root:
+            node = self.root
+            stack = []
+            flag = 1
+            while node:
+                if flag:
+                    print(node.data, end=" ")
+                    if node.left:
+                        stack.append(node)
+                        node = node.left
+                        flag = 1
+                        continue
+                if node.right:
+                    node = node.right
+                    flag = 1
+                else:
+                    try:
+                        node = stack.pop()
+                    except Exception:
+                        return
+                    flag = 0
+        else:
+            print("Tree is Empty!")
+
+    def postorder(self):  # Left Right Root
+        if self.root:
+            node = self.root  # 1 2 4 6 4 2
+            stack = []  # 1
+            flag = 1  # 1 0
+            count = 0
+            while node and count < 100:
+                count += 1
+                if node.left and flag:
+                    stack.append(node)
+                    node = node.left
+                    flag = 1
+                    continue
+                elif node.right and flag:
+                    stack.append(node)                  #    1
+                    node = node.right                   #  /   \
+                    flag = 1                            # 2     3
+                    continue                            #  \     \
+                else:                                   #   4     5
+                    print(node.data)                    #  /     /
+                    try:                                # 6     7
+                        node = stack.pop()
+                    except Exception:
+                        return
+                    flag = 0
+        else:
+            print("Tree is Empty!")
+
+
+# b = BinaryTree()
+# b.display()
+# b.insert(1)
+# b.insert(2)
+# b.insert(3)
+# b.insert(4)
+# b.insert(5)
+# b.insert(6)
+# b.insert(7)
+# b.display()
+
+bt = BinaryTree()
+bt.root = Node(1)
+bt.root.left = Node(2)
+bt.root.right = Node(3)
+bt.root.left.right = Node(4)
+bt.root.left.right.left = Node(6)
+bt.root.right.right = Node(5)
+bt.root.right.right.left = Node(7)
+bt.inorder()
+print()
+bt.preorder()
+print()
+bt.postorder()
